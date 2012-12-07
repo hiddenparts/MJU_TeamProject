c556bad4d43f3aadf12e9f03d42b0cfa2decd348
sangsun <hiddenparts@gmail.com>
2012-12-08 02:31:23 +0900

    delete with ajax

--- src/servlet/CommentServlet.java
+++ src/servlet/CommentServlet.java
@@ -10,7 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
+
 import dao.CommentDAO;
+import dao.GraffitiDAO;
 
 import bean.Article;
 import bean.Comment;
@@ -34,7 +37,35 @@ public class CommentServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		boolean ret = false;
+		JSONObject rsobj = new JSONObject();
+		
+		// 어떤 명령인지 확인
+		String op = request.getParameter("op");
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		try {
+			if(op.equals("remove_comment")) { // 댓글 삭제
+				ret = CommentDAO.remove(id);
+			} else if(op.equals("remove_graffiti")) { // 그림 삭제
+				ret = GraffitiDAO.remove(id);
+			}
+			
+			if(ret) {
+				System.out.println(id + "번 삭제성공");
+				rsobj.put("result", "ok");
+			} else {
+				System.out.println(id + "번 삭제실패");
+				rsobj.put("result", "no");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().print(rsobj.toJSONString());
 	}
 
 	/**
@@ -50,6 +81,7 @@ public class CommentServlet extends HttpServlet {
 		Member user = (Member) session.getAttribute("user"); 		
 		String userid = user.getUserid();
 		String commentcontent = request.getParameter("comment");
+		System.out.println("코멘트내용 : " + commentcontent);
 		//int commentip = request.getParameter("");
 			
 		/* 빈 코멘트를 입력할때 발생하는 에러는 js로 막을 예정 */
@@ -63,9 +95,9 @@ public class CommentServlet extends HttpServlet {
 		try {
 			ret = CommentDAO.create(comment);
 			if(ret) {
-				System.out.println(userid + "님의 코멘트 성공");
+				//System.out.println(userid + "님의 코멘트 성공");
 			} else {
-				System.out.println(userid + "님의 코멘트 실패");
+				//System.out.println(userid + "님의 코멘트 실패");
 			}
 		} catch (Exception e) {
 			System.out.println(e);
